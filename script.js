2543d32d5fd626e6ecc94f9deb45d9f5546241fd
Samuel Ajala <samuelajala01@gmail.com>
2022-06-22 23:22:59 +0100

    chore: add isNaN validation

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const num1 = parseInt(prompt("Enter first number: "), 10)
-const num2 = parseInt(prompt("Enter second number"), 10)
 const operator = prompt("Enter Arithmetic type: ")
+const num2 = parseInt(prompt("Enter second number"), 10)
 
-if (operator === "+") {
-  let ans = num1 + num2
-  alert(`${num1} + ${num2} is ${ans}`)
-} else if (operator === "-") {
-  let ans = num1 - num2
-  alert(`${num1} - ${num2} is ${ans}`)
-} else if (operator === "*") {
-  let ans = num1 * num2
-  alert(`${num1} * ${num2} is ${ans}`)
-} else if (operator === "/") {
-  let ans = num1 / num2
-  alert(`${num1} / ${num2} is ${ans}`)
+if (isNaN(num1) || isNaN(num2)) {
+  alert("Please enter a valid number")
 } else {
-  alert("Sorry, that arithmetic type doesn't work")
+  if (operator === "+") {
+    let ans = num1 + num2
+    alert(`${num1} + ${num2} is ${ans}`)
+  } else if (operator === "-") {
+    let ans = num1 - num2
+    alert(`${num1} - ${num2} is ${ans}`)
+  } else if (operator === "*") {
+    let ans = num1 * num2
+    alert(`${num1} * ${num2} is ${ans}`)
+  } else if (operator === "/") {
+    let ans = num1 / num2
+    alert(`${num1} / ${num2} is ${ans}`)
+  } else {
+    alert("Sorry, that arithmetic type doesn't work")
+  }
 }
